759b317db178c1aa76b546f051452eea6234e765
leoLUIGY <luigyleonidas@gmail.com>
2025-06-22 19:32:36 -0300

    Initial commit of Codeigniter project

---
+++ app/Views/layouts/default.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title><?= $this->renderSection("title") ?></title>
+</head>
+<body>
+    <?= $this->renderSection("content") ?>
+</body>
+</html>
